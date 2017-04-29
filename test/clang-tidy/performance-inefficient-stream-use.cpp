52db1aac53ad7481ba5f826cd5cb9c65a37b40c3
Bittner Barnabás <bittnerbarni@gmail.com>
2017-04-29 18:48:25 +0200

    Added some testcases

--- test/clang-tidy/performance-inefficient-stream-use.cpp
+++ test/clang-tidy/performance-inefficient-stream-use.cpp
@@ -1,14 +1,50 @@
 // RUN: %check_clang_tidy %s performance-inefficient-stream-use %t
 
-// FIXME: Add something that triggers the check here.
-void f();
-// CHECK-MESSAGES: :[[@LINE-1]]:6: warning: function 'f' is insufficiently awesome [performance-inefficient-stream-use]
+namespace std {
+template<class T>
+class basic_ostream {};
 
-// FIXME: Verify the applied fix.
-//   * Make the CHECK patterns specific enough and try to make verified lines
-//     unique to avoid incorrect matches.
-//   * Use {{}} for regular expressions.
-// CHECK-FIXES: {{^}}void awesome_f();{{$}}
+typedef basic_ostream<char> ostream;
+extern ostream cout;
 
-// FIXME: Add something that doesn't trigger the check here.
-void awesome_f2();
+template<class T>
+basic_ostream<T>& endl(basic_ostream<T> &os){ return os; }
+
+ostream& operator<<(ostream& os, const char*){return os;}
+ostream& operator<<(ostream& os, char){ return os;}
+ostream& operator<<(ostream& os, ostream& (*__pf)(ostream&)){return os;}
+}
+
+int main() {
+  std::cout << 'C';
+  std::cout << "A";
+  // CHECK-MESSAGES: :[[@LINE-1]]:16: warning: Inefficient cast from const char[2] to const char *, consider using a character literal [performance-inefficient-stream-use]
+  // CHECK-FIXES: {{.*}}std::cout << 'A';{{$}}
+  std::cout << 'C' << "B" << "A";
+  // CHECK-MESSAGES: :[[@LINE-1]]:23: warning: Inefficient cast
+  // CHECK-MESSAGES: :[[@LINE-2]]:30: warning: Inefficient cast
+  // CHECK-FIXES: {{.*}}std::cout << 'C' << 'B' << 'A';{{$}}
+  std::cout << "A" << "B" << 'C';
+  // CHECK-MESSAGES: :[[@LINE-1]]:16: warning: Inefficient cast
+  // CHECK-MESSAGES: :[[@LINE-2]]:23: warning: Inefficient cast
+  // CHECK-FIXES: {{.*}}std::cout << 'A' << 'B' << 'C';{{$}}
+  std::cout << std::endl << std::endl << std::endl;
+  // CHECK-MESSAGES: :[[@LINE-1]]:16: warning: Multiple std::endl use is not efficient consider using '\n' instead. [performance-inefficient-stream-use]
+  // CHECK-MESSAGES: :[[@LINE-2]]:29: warning: Multiple std::endl
+  // CHECK-FIXES: {{.*}}std::cout << '\n' << '\n' << std::endl;{{$}}
+  std::cout << std::endl;
+  std::cout << std::endl << std::endl;
+  // CHECK-MESSAGES: :[[@LINE-1]]:16: warning: Multiple std::endl
+  // CHECK-FIXES: {{.*}}std::cout << '\n' << std::endl;{{$}}
+  std::cout << "A" << std::endl;
+  // CHECK-MESSAGES: :[[@LINE-1]]:16: warning: Inefficient cast
+  // CHECK-FIXES: {{.*}}std::cout << 'A' << std::endl;{{$}}
+  std::cout << "A" << std::endl << std::endl;
+  // CHECK-MESSAGES: :[[@LINE-1]]:16: warning: Inefficient cast
+  // CHECK-MESSAGES: :[[@LINE-2]]:23: warning: Multiple std::endl
+  // CHECK-FIXES: {{.*}}std::cout << 'A' << '\n' << std::endl;{{$}}
+  std::cout << "A" << 'B' << std::endl << std::endl;
+  // CHECK-MESSAGES: :[[@LINE-1]]:16: warning: Inefficient cast
+  // CHECK-MESSAGES: :[[@LINE-2]]:30: warning: Multiple std::endl
+  // CHECK-FIXES: {{.*}}std::cout << 'A' << 'B' << '\n' << std::endl;{{$}}
+}
